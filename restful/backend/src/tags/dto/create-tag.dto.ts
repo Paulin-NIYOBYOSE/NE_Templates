ba294bfe94b46e581ecd7c71516900bd44d0bd10
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2026-05-28 21:10:58 +0200

    chore: remove obsolete restful/backend directory (restructured into monolithic/)

--- restful/backend/src/tags/dto/create-tag.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateTagDto {
-  @ApiProperty({ example: 'electronics', description: 'Tag name (unique)' })
-  @IsString()
-  @MinLength(2)
-  name: string;
-}
